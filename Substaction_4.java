8bb2a5b1ddef8903e87bd52f173f20727b04c319
Jyoti-74 <jyotiprakashsahoo74@gmail.com>
2025-09-04 22:55:48 +0530

    Add files via upload

---
+++ Substaction_4.java
@@ -0,0 +1,9 @@
+public class Substaction_4 {
+    public static void main(String[]args){
+        int a = 30;
+        int b = 20;
+        int difference = a - b;
+        System.out.println("The difference of a and b is: " + difference);
+    }   
+    
+}

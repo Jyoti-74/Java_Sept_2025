c30bb814e9e70e9b60daf5c6b53cf85a66405769
Jyoti-74 <jyotiprakashsahoo74@gmail.com>
2025-09-07 19:27:00 +0530

    Add files via upload

---
+++ Multiplication_6.java
@@ -0,0 +1,9 @@
+public class Multiplication_6 {
+    public static void main(String [] args){
+        int a=5;
+        int b=10;
+        int product=a*b;
+        System.out.println("The product of first and second number is:" + product);
+    }
+    
+}

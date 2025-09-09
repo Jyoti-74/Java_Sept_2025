50471c12f1005330e3ccf453818f36df9dae4852
Jyoti-74 <jyotiprakashsahoo74@gmail.com>
2025-09-09 21:01:25 +0530

    Add files via upload

---
+++ Division_8.java
@@ -0,0 +1,8 @@
+public class Division_8 {
+    public static void main(String [] args){
+        int a=20;
+        int b=5;
+        int division=a/b;
+        System.out.println("The division of first and second number is:" + division);
+    }
+}

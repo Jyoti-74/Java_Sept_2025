e7c0d591b8a00b7d242e8551ec5d75d5af4df0a0
Jyoti-74 <jyotiprakashsahoo74@gmail.com>
2025-09-09 20:59:20 +0530

    Add files via upload

---
+++ Sc_multiplication_7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Sc_multiplication_7{
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number:");
+        int num2 = sc.nextInt();
+        int result= num1 * num2;
+        System.out.println("The multiplication between first number & second number is:"+ result);
+
+    }
+}
+

a52a3d32a48608f278a0b1f4fb181bab7ee511cf
Jyoti-74 <jyotiprakashsahoo74@gmail.com>
2025-09-10 22:22:26 +0530

    Add files via upload

---
+++ Sc_division_9.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sc_division_9{
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        int c= a/b;
+        System.out.println("The division is:" + c);
+    }
+}

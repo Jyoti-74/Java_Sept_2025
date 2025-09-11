3fca121de38de228062c29d6d02b576e113fb4f2
Jyoti-74 <jyotiprakashsahoo74@gmail.com>
2025-09-11 23:33:22 +0530

    Add files via upload

---
+++ Percentage_10.java
@@ -0,0 +1,8 @@
+public class Percentage_10 {
+    public static void main(String [] args){
+        int a= 27;
+        int b= 50;
+        int c= (a*100)/b;
+        System.out.println("The percentage is:" + c);
+    }
+}

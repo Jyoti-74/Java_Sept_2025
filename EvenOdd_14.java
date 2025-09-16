bce92d9087e2251aa76fe0c4420320135e0d6e36
Jyoti-74 <jyotiprakashsahoo74@gmail.com>
2025-09-16 16:02:59 +0530

    Add files via upload

---
+++ EvenOdd_14.java
@@ -0,0 +1,10 @@
+public class EvenOdd_14 {
+    public static void main(String[] args) {
+        int num = 29;
+        if (num % 2 == 0) {
+            System.out.println(num + " is an Even number");
+        } else {
+            System.out.println(num + " is an Odd number");
+        }
+    }
+}
